49c240b0875c20e99d20fe8372ec241166c98cd3
Pola <83130305+polalove@users.noreply.github.com>
2024-07-26 17:24:30 +0300

    132312

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 import os
-# sadasdasdas
+
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.enums import ParseMode
 from aiogram.filters.command import Command
